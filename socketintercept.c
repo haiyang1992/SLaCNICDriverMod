e5f70c4e7d7675ad4d4023f087ee1ec190701e24
haiyang1992 <haiyang1992@gmail.com>
2016-04-21 15:54:07 -0500

    sendmsg syscall

--- socketintercept.c
+++ socketintercept.c
@@ -19,10 +19,10 @@ unsigned long *sys_call_table;
 
 extern void e1000_laser_init(struct net_device *netdev);
 extern void e1000_laser_sock_close(struct net_device *netdev);
-extern int getsockname(int sockfd, struct sockaddr *addr, int *addrlen);
+//extern int getsockname(int sockfd, struct sockaddr *addr, int *addrlen);
 
-asmlinkage int (*original_socket) (int domain, int type, int protocol);
-asmlinkage int e1000_socket(int domain, int type, int protocol)
+asmlinkage int (*original_sendmsg) (int sockfd, struct mmsghdr *msgvec, unsigned int vlen, unsigned int flags);
+asmlinkage int e1000_sendmsg(int sockfd, struct mmsghdr *msgvec, unsigned int vlen, unsigned int flags)
 {
     struct net_device *dev;
     printk(KERN_INFO "socket() intercepted\nNow turning on laser\n"); 
@@ -31,44 +31,44 @@ asmlinkage int e1000_socket(int domain, int type, int protocol)
     printk(KERN_INFO"@ [%s]\n", dev->name);
     if (dev->name == "eth0") {
         printk(KERN_INFO "Now turning on laser\n");
-        e1000_laser_init(dev);
+        //e1000_laser_init(dev);
     }
     else {
         printk(KERN_INFO "NIC not e1000!\n");
     }
     
-    return original_socket(domain, type, protocol);
+    return original_sendmsg(sockfd, msgvec, vlen, flags);
 };
 
-asmlinkage long (*original_close) (unsigned int fd);
-asmlinkage long e1000_close(unsigned int fd)
-{ 
-    struct sockaddr addr;
-    int addrlen = sizeof(addr);
-    printk(KERN_INFO "close() intercepted\nNow turning off laser\n");
-    //if (getsockname(fd, &addr, &addrlen) == -1)
-    if (getsockname(fd, &addr, &addrlen) == -1)
-    {
-        printk(KERN_INFO"not a socket close\n");
-        return original_close(fd);
-    }
-    else
-    {
-        struct net_device *dev;
-        dev = first_net_device(&init_net);
-        // assume current dev is eth0, using e1000
-        printk(KERN_INFO"@ [%s]\n", dev->name);
-        if (dev->name == "eth0") {
-            printk(KERN_INFO "Running laser sock close\n");
-            e1000_laser_sock_close(dev);
-        }
-        else {
-            printk(KERN_INFO "NIC not e1000!\n");
-        }
+/*asmlinkage long (*original_close) (unsigned int fd);*/
+/*asmlinkage long e1000_close(unsigned int fd)*/
+/*{ */
+    /*struct sockaddr addr;*/
+    /*int addrlen = sizeof(addr);*/
+    /*printk(KERN_INFO "close() intercepted\nNow turning off laser\n");*/
+    /*//if (getsockname(fd, &addr, &addrlen) == -1)*/
+    /*if (getsockname(fd, &addr, &addrlen) == -1)*/
+    /*{*/
+        /*printk(KERN_INFO"not a socket close\n");*/
+        /*return original_close(fd);*/
+    /*}*/
+    /*else*/
+    /*{*/
+        /*struct net_device *dev;*/
+        /*dev = first_net_device(&init_net);*/
+        /*// assume current dev is eth0, using e1000*/
+        /*printk(KERN_INFO"@ [%s]\n", dev->name);*/
+        /*if (dev->name == "eth0") {*/
+            /*printk(KERN_INFO "Running laser sock close\n");*/
+            /*e1000_laser_sock_close(dev);*/
+        /*}*/
+        /*else {*/
+            /*printk(KERN_INFO "NIC not e1000!\n");*/
+        /*}*/
     
-        return original_close(fd);
-     }
-};
+        /*return original_close(fd);*/
+     /*}*/
+/*};*/
 
 static int __init sock_intercept_init(void)
 {
@@ -77,16 +77,16 @@ static int __init sock_intercept_init(void)
     *(long *)&sys_call_table = 0xffffffff81a001c0;
 
     /* store original location of socket(). Alter sys_call_table to point to our functions*/
-    original_socket = (void *)xchg(&sys_call_table[__NR_socket], e1000_socket);
-    original_close = (void *)xchg(&sys_call_table[__NR_close], e1000_close);
+    original_sendmsg = (void *)xchg(&sys_call_table[__NR_sendmsg], e1000_sendmsg);
+//    original_close = (void *)xchg(&sys_call_table[__NR_close], e1000_close);
     return 0;
 }
 
 static void __exit sock_intercept_exit(void)
 {
     /* restore original socket() in sys_call_table */
-    xchg(&sys_call_table[__NR_socket], original_socket);
-    xchg(&sys_call_table[__NR_close], original_close);
+    xchg(&sys_call_table[__NR_sendmsg], original_sendmsg);
+  //  xchg(&sys_call_table[__NR_close], original_close);
 
     printk(KERN_INFO "socket call intercept exiting\n");
 }
